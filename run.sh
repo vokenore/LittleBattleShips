37901bf90b6fa5f523cc3df7f612116f0b90a0b7
Yulian8 <ysafronnikov8@gmail.com>
2026-06-16 21:54:53 +0300

    Finally...YES... (x2) it's very incorrect commit message, but... yes....

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p out
-javac -d out -cp "lib/Java-WebSocket-1.6.0.jar:lib/slf4j-api-2.0.9.jar:lib/slf4j-simple-2.0.9.jar" src/com.littlebattleships/*.java
+javac -d out -cp "lib/Java-WebSocket-1.6.0.jar:lib/slf4j-api-2.0.9.jar:lib/slf4j-simple-2.0.9.jar" src/com/littlebattleships/*.java
 java -cp "out:lib/Java-WebSocket-1.6.0.jar:lib/slf4j-api-2.0.9.jar:lib/slf4j-simple-2.0.9.jar" com.littlebattleships.Main
 run.bat:
